b73c4d3a6a0d0354af9d119a7b2eaf491c3796ca
rw-filho <rwrw3011@gmail.com>
2020-06-18 13:29:49 -0300

    I've made a program to help a company

---
+++ Project-Test01/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Product_Test
+{
+    class Product
+    {
+        public string Name;
+        public double Price;
+        public int Quantity;
+
+        public double TotalValueInStock()
+        {
+            return Price * Quantity;
+        }
+
+        public void AddProducts(int quantidade)
+        {
+            Quantity += quantidade;
+        }
+        public void RemoveProducts(int quantidade)
+        {
+            Quantity -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", $ " + Price.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantity + " Unidades , " + "Total: $ " + TotalValueInStock().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Project-Test01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+namespace Product_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the Product's Data: ");
+            Product product = new Product();
+            Console.Write("Name: ");
+            product.Name = Console.ReadLine();
+            Console.Write("Price: ");
+            product.Price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantity in stock: ");
+            product.Quantity = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Product's Data: ");
+            Console.WriteLine(product);
+            Console.WriteLine();
+
+            Console.Write("Type the number of products to be added to the stock:  ");
+            int ExtraQuantity = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            product.AddProducts(ExtraQuantity);
+
+            Console.Write("Updated Data: ");
+            Console.Write(product);
+            Console.WriteLine();
+
+            Console.WriteLine();
+
+            Console.Write("Type the number of products to be removed from the stock: ");
+            int QuantityRemove = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            product.RemoveProducts(QuantityRemove);
+
+            Console.Write("Updated Data: ");
+            Console.WriteLine(product);
+        }
+    }
+}
